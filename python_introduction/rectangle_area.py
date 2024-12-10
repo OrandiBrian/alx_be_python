01c160ea5e1057a34dac2b2170e504692a424be4
Brian Orandi <brianorandi97@gmail.com>
2024-12-10 08:35:07 +0300

    Area of a rectagle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Python script that calculates the area of a rectangle"""
+
+# variables
+length = 10
+width = 5
+
+# area
+area = length * width
+
+# result
+print(f"The area of the rectangle is: {area}")
