8c548a80ffbf07c06297197618f1d0785d1b57f0
Brian Orandi <brianorandi97@gmail.com>
2024-12-10 08:26:24 +0300

    Python basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#!/bin/bash/python3
+
+number1 = 10
+number2 = 5
+
+addition = number1 + number2 # Addition
+difference = number1 - number2 # subtraction
+product = number1 * number2 # product
+
+# printing the output
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
