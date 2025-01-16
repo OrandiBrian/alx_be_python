8ccea1788ac9a01e84805273c1db6d6835deb51b
Brian Orandi <brianorandi97@gmail.com>
2025-01-16 08:50:13 +0300

    Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_balance=0):
+    def __init__(self, account_balance):
         self.account_balance = account_balance
 
     def deposit(self, amount):
@@ -13,4 +13,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
